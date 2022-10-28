badeeca99cec444424e284bfb9f5c51513638453
AyaobaEstelle <abolarinesther98@gmail.com>
2022-10-28 23:33:56 +0100

    add week 4 assignments

---
+++ Number gussing game/script.js
@@ -0,0 +1,17 @@
+let upper = 2;
+let name = prompt('Enter username:')
+
+
+const play = function() {
+  while (true) {
+    let guess = prompt(`${name} Enter a number between 1 - ${upper}: (stage ${upper - 1}) `);
+    let random = Math.ceil(Math.random() * upper)
+    if (guess == random) {
+      upper++
+      console.log(`Correct Levelling Up! ${name}`)
+    } else {
+      console.log(`Wrong Guess, Sorry ${name} Game Over. you guessed`, guess, '. The correct answer is', random)
+      break
+    }
+  }
+}
